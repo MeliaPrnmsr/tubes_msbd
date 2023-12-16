060f56eb3d2986f4e114493cc4af48e2b4606d14
Sintong23 <sintongjohannes1@gmail.com>
2023-12-16 14:24:08 +0700

    trigger2

--- resources/views/admin/log.blade.php
+++ resources/views/admin/log.blade.php
@@ -8,14 +8,16 @@
     <div class="card-body">
       <div class="row d-flex justify-content-center">  
         <div class="col-8">
-          <div class="input-group">
-            <input class="form-control border-end-0 border rounded-pill" type="text" value="search" id="example-search-input">
-            <span class="input-group-append">
-                <button class="btn btn-outline-secondary bg-white border-start-0 border rounded-pill ms-n3" type="button">
-                    <i class="fa fa-search"></i>
-                </button>
-            </span>
-          </div>     
+          <form action="/log_admin" method="GET">
+            <div class="input-group">
+              <input class="form-control border-end-0 border rounded-pill" type="text" value="{{ request('search') }}" id="example-search-input" name="search">
+              <span class="input-group-append">
+                  <button class="btn btn-outline-secondary bg-white border-start-0 border rounded-pill ms-n3" type="submit">
+                      <i class="fa fa-search"></i>
+                  </button>
+              </span>
+            </div>   
+            </form>
         </div>
   
       </div>
@@ -23,63 +25,41 @@
     <br>
   
       {{-- tabel daftar mhs start --}}
-      <div class="card-body mx-auto justify-content-center" style="width: 90%">
+      <div class="card-body mx-auto justify-content-center" style="width: 95%">
         <div class="row bg-hijau shadow p-3 mb-2">
           <div class="col-1">No</div>
-          <div class="col-6">Tindakan</div>
-          <div class="col-3 text-center">Waktu</div>
-          <div class="col-2 text-center">Aksi</div>
+          <div class="col-2">Tindakan</div>
+          <div class="col-2  ">Waktu</div>
+          <div class="col-2 ">Sebelum</div>
+          <div class="col-2 ">Sesudah</div>
+          <div class="col-2 ">Deskripsi</div>
         </div>
+        @php
+            $i=1;
+        @endphp
   
-        <div class="row shadow p-3 mb-2 align-items-center">
-          <div class="col-1">1</div>
-          <div class="col-6">Insert dan Update pada skripsi</div>
-          <div class="col-3 text-center">02-12-2023</div>
-          <div class="col-2 text-center">
-            <a href="/detailmahasiswastaff" class="btn btn-repository">Detail</a>
-          </div>
+        @foreach ($cariLog as $data)
+          <div class="row shadow p-3 mb-2 align-items-center">
+          <div class="col-1">{{ $i }}</div>
+          <div class="col-2">{{ $data->action }}</div>
+          <div class="col-2 ">{{ $data->waktu }}</div>
+          <div class="col-2">{{ $data->sebelum }}</div>
+          <div class="col-2">{{ $data->sesudah }}</div>
+          <div class="col-2">{{ $data->deskripsi }}</div>
+
         </div>
-  
-        <div class="row shadow p-3 mb-2 align-items-center">
-          <div class="col-1">2</div>
-          <div class="col-6">Kim Min Seok</div>
-          <div class="col-3 text-center">02-12-2023</div>
-          <div class="col-2 text-center">
-            <a href="/detailmahasiswastaff" class="btn btn-repository">Detail</a>
-          </div>
-        </div>
-  
-        <div class="row shadow p-3 mb-2 align-items-center">
-          <div class="col-1">3</div>
-          <div class="col-6">Inser dan Update pada skripsi</div>
-          <div class="col-3 text-center">02-12-2023</div>
-          <div class="col-2 text-center">
-            <a href="/detailmahasiswastaff" class="btn btn-repository">Detail</a>
-          </div>
-        </div>
+        @php
+            $i++;
+        @endphp
+        @endforeach
+        
   
       </div>
       {{-- tabel daftar mhs end --}}
       <br>
       {{-- pagination start --}}
       <div class="d-flex justify-content-center card-body">
-        <nav aria-label="Page navigation example">
-          <ul class="pagination">
-            <li class="page-item">
-              <a class="page-link" href="#" aria-label="Previous" style="color: #3dae2b">
-                <span aria-hidden="true">&laquo;</span>
-              </a>
-            </li>
-            <li class="page-item"><a class="page-link" href="#"  style="color: #3dae2b">1</a></li>
-            <li class="page-item"><a class="page-link" href="#"  style="color: #3dae2b">2</a></li>
-            <li class="page-item"><a class="page-link" href="#"  style="color: #3dae2b">3</a></li>
-            <li class="page-item">
-              <a class="page-link" href="#" aria-label="Next"  style="color: #3dae2b">
-                <span aria-hidden="true">&raquo;</span>
-              </a>
-            </li>
-          </ul>
-        </nav>
+        {{ $cariLog->links() }}
       </div>
       {{-- pagination end --}}
   </div>
